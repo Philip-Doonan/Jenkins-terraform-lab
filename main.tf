2a499b9798a905973fc9152fc72c66fa9530a3f4
Philip Doonan <pdoonan10@gmail.com>
2026-01-22 11:57:58 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket         = "{your state bucket}"   
+    bucket         = "jenkins-state-philip1"   
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
 }
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "philip567" {
 bucket = "jenkins-test-bucket-{yournamehere}"
 }
